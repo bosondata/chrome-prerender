28a692b55f5ab5ed70664bab8206fe943cbbf6e9
messense <messense@icloud.com>
2017-03-08 20:22:42 +0800

    Add window.prerenderReady check

--- chromerdp.py
+++ chromerdp.py
@@ -11,14 +11,15 @@ logger = logging.getLogger(__name__)
 
 
 class ChromeRemoteDebugger:
-    def __init__(self, host, port):
+    def __init__(self, host, port, loop=None):
         self._debugger_url = 'http://{}:{}'.format(host, port)
-        self._session = aiohttp.ClientSession()
+        self._session = aiohttp.ClientSession(loop=loop)
+        self.loop = loop
 
     async def tabs(self):
         async with self._session.get('{}/json/list'.format(self._debugger_url)) as res:
             tabs = await res.json(loads=json.loads)
-            return [Tab(self, tab) for tab in tabs]
+            return [Tab(self, tab, loop=self.loop) for tab in tabs if 'webSocketDebuggerUrl' in tab]
 
     async def new_tab(self, url=None):
         endpoint = '{}/json/new'.format(self._debugger_url)
@@ -40,18 +41,18 @@ class ChromeRemoteDebugger:
 
 
 class Tab:
-    def __init__(self, debugger, tab_info):
+    def __init__(self, debugger, tab_info, loop=None):
         self._debugger = debugger
-        self.description = tab_info['description']
+        self.loop = loop
         self.id = tab_info['id']
-        self.type = tab_info['type']
-        self.title = tab_info['title']
-        self.url = tab_info['url']
-        self.devtools_frontend_url = tab_info['devtoolsFrontendUrl']
         self.websocket_debugger_url = tab_info['webSocketDebuggerUrl']
         self.websocket = None
-        self._request_id = -1
+        self._request_id = 0
         self._get_html_request_id = -1
+        self._eval_request_ids = set()
+        self._load_event_fired = False
+        self._prerender_ready = False
+        self._get_document_request_id = -1
 
     @property
     def next_request_id(self):
@@ -59,68 +60,95 @@ class Tab:
         return self._request_id
 
     async def attach(self):
-        self.websocket = await websockets.connect(self.websocket_debugger_url)
-        await self.websocket.send(json.dumps({
+        logger.info('Connecting to %s', self.websocket_debugger_url)
+        self.websocket = await websockets.connect(self.websocket_debugger_url, loop=self.loop)
+
+    async def listen(self):
+        await self.send({
             'id': self.next_request_id,
             'method': 'Page.enable'
-        }))
-        await self.websocket.recv()
-        await self.websocket.send(json.dumps({
+        })
+        await self.recv()
+        await self.send({
             'id': self.next_request_id,
             'method': 'Network.enable'
-        }))
-        await self.websocket.recv()
+        })
+        await self.recv()
+
+    async def send(self, payload):
+        req_id = payload.get('id') or self.next_request_id
+        payload['id'] = req_id
+        return await self.websocket.send(json.dumps(payload))
+
+    async def recv(self):
+        res = await self.websocket.recv()
+        return json.loads(res)
 
     async def set_user_agent(self, ua):
-        await self.websocket.send(json.dumps({
-            'id': self.next_request_id,
+        await self.send({
             'method': 'Network.setUserAgentOverride',
             'params': {'userAgent': ua}
-        }))
-        await self.websocket.recv()
+        })
+        await self.recv()
 
     async def navigate(self, url):
-        await self.websocket.send(json.dumps({
-            'id': self.next_request_id,
+        await self.send({
             'method': 'Page.navigate',
             'params': {'url': url}
-        }))
-        await self.websocket.recv()
+        })
+        await self.recv()
 
     async def evaluate(self, expr):
-        await self.websocket.send(json.dumps({
-            'id': self.next_request_id,
+        request_id = self.next_request_id
+        self._eval_request_ids.add(request_id)
+        await self.send({
+            'id': request_id,
             'method': 'Runtime.evaluate',
             'params': {'expression': expr}
-        }))
+        })
 
     async def wait(self):
+        await self.send({
+            'id': self.next_request_id,
+            'method': 'Network.disable'
+        })
         while True:
-            # await self.evaluate('window.prerenderReady === true')
-            message = await self.websocket.recv()
-            obj = json.loads(message)
+            obj = await self.recv()
             method = obj.get('method')
             if method == 'Page.loadEventFired':
-                await self.get_html()
+                self._load_event_fired = True
+            if not self._prerender_ready and self._load_event_fired:
+                await self.evaluate('window.prerenderReady === true')
+                await asyncio.sleep(0.5)
             req_id = obj.get('id')
-            if req_id == self._get_html_request_id:
+            if req_id is None:
+                continue
+            if req_id in self._eval_request_ids:
+                if obj['result']['result']['value']:
+                    self._prerender_ready = True
+                    self._eval_request_ids.clear()
+                    await self.get_document()
+            elif req_id == self._get_document_request_id:
+                node_id = obj['result']['root']['nodeId']
+                await self.get_html(node_id)
+            elif req_id == self._get_html_request_id:
                 html = obj['result']['outerHTML']
                 return html
 
-    async def get_html(self):
-        await self.websocket.send(json.dumps({
-            'id': self.next_request_id,
+    async def get_document(self):
+        self._get_document_request_id = self.next_request_id
+        await self.send({
+            'id': self._get_document_request_id,
             'method': 'DOM.getDocument',
-        }))
-        res = await self.websocket.recv()
-        # root_node_id = json.loads(res)['result']['root']['nodeId']
-        root_node_id = 1
+        })
+
+    async def get_html(self, node_id):
         self._get_html_request_id = self.next_request_id
-        await self.websocket.send(json.dumps({
+        await self.send({
             'id': self._get_html_request_id,
             'method': 'DOM.getOuterHTML',
-            'params': {'nodeId': root_node_id}
-        }))
+            'params': {'nodeId': node_id}
+        })
 
     async def close(self):
         return await self._debugger.close_tab(self.id)
--- prerender.py
+++ prerender.py
@@ -2,14 +2,18 @@
 
 import sys
 import asyncio
+import logging
 import logging.config
+from signal import signal, SIGINT
 
+import uvloop
+import ujson as json
 from sanic import Sanic
 from sanic import response
 from sanic.exceptions import NotFound
 from async_timeout import timeout
 
-from chromerdp import ChromeRemoteDebugger
+from chromerdp import ChromeRemoteDebugger, Tab
 
 LOGGING = {
     'version': 1,
@@ -33,21 +37,62 @@ LOGGING = {
     },
 }
 logging.config.dictConfig(LOGGING)
+logger = logging.getLogger(__name__)
 
 
-async def prerender(url):
-    rdp = ChromeRemoteDebugger('localhost', 9222)
-    # tabs = await rdp.tabs()
-    # tab = tabs[0]
-    tab = await rdp.new_tab()
+class Prerender:
+    def __init__(self, host='localhost', port=9222, loop=None):
+        self.host = host
+        self.port = port
+        self.loop = loop
+        self._rdp = ChromeRemoteDebugger(host, port, loop=loop)
+        self._ctrl_tab = None
+
+    async def connect(self):
+        tabs = await self._rdp.tabs()
+        self._ctrl_tab = tabs[0]
+        await self._ctrl_tab.attach()
+        logger.info('Connected to control tab %s', self._ctrl_tab.id)
+
+    async def new_tab(self, url=None):
+        await self._ctrl_tab.send({
+            'method': 'Target.createTarget',
+            'params': {
+                'url': url or 'about:blank'
+            }
+        })
+        res = await self._ctrl_tab.recv()
+        tab_id = res['result']['targetId']
+        logger.info('Created new tab %s', tab_id)
+        tabs = await self._rdp.tabs()
+        tab = [tb for tb in tabs if tb.id == tab_id][0]
+        return tab
+
+    async def close_tab(self, tab_id):
+        await self._ctrl_tab.send({
+            'method': 'Target.closeTarget',
+            'params': {'targetId': tab_id}
+        })
+        res = await self._ctrl_tab.recv()
+
+    async def close(self):
+        tabs = await self._rdp.tabs()
+        for tab in tabs:
+            await tab.close()
+        logger.info('All tabs closed')
+
+
+async def prerender(renderer, url):
+    tab = await renderer.new_tab()
     await tab.attach()
+    await tab.listen()
     await tab.set_user_agent('Mozilla/5.0 (Linux) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3033.0 Safari/537.36 Prerender (bosondata)')  # NOQA
     try:
         await tab.navigate(url)
         with timeout(30):
             html = await tab.wait()
     finally:
-        await tab.close()
+        await renderer.close_tab(tab.id)
     return html
 
 
@@ -60,10 +105,16 @@ async def handle_request(request, exception):
     if url.startswith('/http'):
         url = url[1:]
     try:
-        html = await prerender(url)
+        html = await prerender(request.app.prerender, url)
         return response.text(html)
     except asyncio.TimeoutError:
         return response.text('Gateway timeout', status=504)
+
+
+@app.listener('after_server_start')
+def after_server_start(app, loop):
+    app.prerender = Prerender(loop=loop)
+    loop.run_until_complete(app.prerender.connect())
 
 
 if __name__ == '__main__':
