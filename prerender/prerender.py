579edd5933a659a585a87f778cdc6bcd403203cf
messense <messense@icloud.com>
2017-04-08 11:52:08 +0800

    Track Chrome tab ids

--- prerender/prerender.py
+++ prerender/prerender.py
@@ -4,7 +4,7 @@ import logging
 from multiprocessing import cpu_count
 
 from async_timeout import timeout
-from websockets.exceptions import InvalidHandshake
+from websockets.exceptions import InvalidHandshake, ConnectionClosed
 
 from .chromerdp import ChromeRemoteDebugger
 
@@ -21,12 +21,14 @@ class Prerender:
         self.port = port
         self.loop = loop
         self._rdp = ChromeRemoteDebugger(host, port, loop=loop)
+        self._tab_ids = set()
         self._idle_tabs = asyncio.Queue(loop=self.loop)
 
     async def bootstrap(self):
         for i in range(CONCURRENCY_PER_WORKER):
             tab = await self._rdp.new_tab()
             await self._idle_tabs.put(tab)
+            self._tab_ids.add(tab.id)
 
     async def tabs(self):
         return await self._rdp.tabs()
@@ -35,14 +37,13 @@ class Prerender:
         return await self._rdp.version()
 
     async def shutdown(self):
-        try:
-            while True:
-                tab = self._idle_tabs.get_nowait()
-                await tab.close()
-        except asyncio.QueueEmpty:
-            pass
+        for tab_id in self._tab_ids:
+            await self._rdp.close_tab(tab_id)
 
     async def render(self, url):
+        if not self._tab_ids:
+            raise RuntimeError('No browser available')
+
         tab = await self._idle_tabs.get()
         reopen = False
         try:
@@ -54,6 +55,10 @@ class Prerender:
             return html
         except InvalidHandshake:
             logger.error('Chrome invalid handshake for tab %s', tab.id)
+            reopen = True
+            raise
+        except ConnectionClosed:
+            logger.error('Chrome remote connection closed for tab %s', tab.id)
             reopen = True
             raise
         except RuntimeError as e:
@@ -78,7 +83,9 @@ class Prerender:
             return
 
         await tab.close()
+        self._tab_ids.remove(tab.id)
         tab = await self._rdp.new_tab()
         # wait until Chrome is ready
         await asyncio.sleep(0.5)
         await self._idle_tabs.put(tab)
+        self._tab_ids.add(tab.id)
