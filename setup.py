1e5232bc27d5096c9eae390cf4ec4e6101b78f2d
messense <messense@icloud.com>
2017-04-09 21:32:40 +0800

    Bump version: 0.4.1 → 0.4.2

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='prerender',
-    version='0.4.1',
+    version='0.4.2',
     author='messense',
     author_email='messense@icloud.com',
     packages=find_packages(exclude=('tests', 'tests.*')),
