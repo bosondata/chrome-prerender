65c0b9715255d323ebcf54ad3ef70626c62db7eb
messense <messense@icloud.com>
2017-05-09 09:57:54 +0800

    Revert "Disable keep-alive"
    This reverts commit 244cf69ebd5fb567f79a3b0da0ea7d8caca26df6.

--- prerender/app.py
+++ prerender/app.py
@@ -48,9 +48,6 @@ def _save_to_cache(key: str, data: bytes, format: str = 'html') -> None:
 
 
 app = Sanic(__name__)
-app.config.from_object(dict(
-    KEEP_ALIVE=False,
-))
 
 
 @app.route('/browser/list')
